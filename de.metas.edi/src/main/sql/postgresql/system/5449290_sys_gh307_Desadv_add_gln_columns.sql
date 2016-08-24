61a4d0fc3693092106342eec0ef28d0d87ba1d3e
Tobias Schöneberg <tobias.schoeneberg@metasfresh.com>
2016-08-24 07:42:10 +0200

    #307 SQL encoding fix

--- de.metas.edi/src/main/sql/postgresql/system/5449290_sys_gh307_Desadv_add_gln_columns.sql
+++ de.metas.edi/src/main/sql/postgresql/system/5449290_sys_gh307_Desadv_add_gln_columns.sql
@@ -1,4 +1,4 @@
--- 23.08.2016 11:37
+Ôªø-- 23.08.2016 11:37
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
 UPDATE AD_Element SET ColumnName='EDISenderIdentification', Description='EDI-GLN des Absenders', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.', Name='Absender-GLN', PrintName='Absender-GLN',Updated=TO_TIMESTAMP('2016-08-23 11:37:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=541984
 ;
@@ -35,7 +35,7 @@ UPDATE AD_PrintFormatItem pi SET PrintName='Absender-GLN', Name='Absender-GLN' W
 
 -- 23.08.2016 11:38
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Element SET ColumnName='EDIReceiverIdentification', Description='EDI-GLN des Empf‰ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.', Name='Empf‰nger-GLN', PrintName='Empf‰nger-GLN',Updated=TO_TIMESTAMP('2016-08-23 11:38:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=541983
+UPDATE AD_Element SET ColumnName='EDIReceiverIdentification', Description='EDI-GLN des Empf√§ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.', Name='Empf√§nger-GLN', PrintName='Empf√§nger-GLN',Updated=TO_TIMESTAMP('2016-08-23 11:38:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=541983
 ;
 
 -- 23.08.2016 11:38
@@ -45,27 +45,27 @@ UPDATE AD_Element_Trl SET IsTranslated='N' WHERE AD_Element_ID=541983
 
 -- 23.08.2016 11:38
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Column SET ColumnName='EDIReceiverIdentification', Name='Empf‰nger-GLN', Description='EDI-GLN des Empf‰ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.' WHERE AD_Element_ID=541983
+UPDATE AD_Column SET ColumnName='EDIReceiverIdentification', Name='Empf√§nger-GLN', Description='EDI-GLN des Empf√§ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.' WHERE AD_Element_ID=541983
 ;
 
 -- 23.08.2016 11:38
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Process_Para SET ColumnName='EDIReceiverIdentification', Name='Empf‰nger-GLN', Description='EDI-GLN des Empf‰ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.', AD_Element_ID=541983 WHERE UPPER(ColumnName)='EDIRECEIVERIDENTIFICATION' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+UPDATE AD_Process_Para SET ColumnName='EDIReceiverIdentification', Name='Empf√§nger-GLN', Description='EDI-GLN des Empf√§ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.', AD_Element_ID=541983 WHERE UPPER(ColumnName)='EDIRECEIVERIDENTIFICATION' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
 ;
 
 -- 23.08.2016 11:38
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Process_Para SET ColumnName='EDIReceiverIdentification', Name='Empf‰nger-GLN', Description='EDI-GLN des Empf‰ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.' WHERE AD_Element_ID=541983 AND IsCentrallyMaintained='Y'
+UPDATE AD_Process_Para SET ColumnName='EDIReceiverIdentification', Name='Empf√§nger-GLN', Description='EDI-GLN des Empf√§ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.' WHERE AD_Element_ID=541983 AND IsCentrallyMaintained='Y'
 ;
 
 -- 23.08.2016 11:38
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Field SET Name='Empf‰nger-GLN', Description='EDI-GLN des Empf‰ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=541983) AND IsCentrallyMaintained='Y'
+UPDATE AD_Field SET Name='Empf√§nger-GLN', Description='EDI-GLN des Empf√§ngers', Help='Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=541983) AND IsCentrallyMaintained='Y'
 ;
 
 -- 23.08.2016 11:38
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_PrintFormatItem pi SET PrintName='Empf‰nger-GLN', Name='Empf‰nger-GLN' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=541983)
+UPDATE AD_PrintFormatItem pi SET PrintName='Empf√§nger-GLN', Name='Empf√§nger-GLN' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=541983)
 ;
 
 -- 23.08.2016 11:39
@@ -86,7 +86,7 @@ UPDATE AD_Column SET IsMandatory='Y',Updated=TO_TIMESTAMP('2016-08-23 11:39:51',
 
 -- 23.08.2016 11:40
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,AllowZoomTo,ColumnName,Created,CreatedBy,DDL_NoForeignKey,Description,EntityType,FieldLength,Help,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsCalculated,IsDimension,IsEncrypted,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsSelectionColumn,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,555010,541983,0,10,540644,'N','EDIReceiverIdentification',TO_TIMESTAMP('2016-08-23 11:40:03','YYYY-MM-DD HH24:MI:SS'),100,'N','EDI-GLN des Empf‰ngers','de.metas.esb.edi',14,'Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.','Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N','Empf‰nger-GLN',0,TO_TIMESTAMP('2016-08-23 11:40:03','YYYY-MM-DD HH24:MI:SS'),100,0)
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,AllowZoomTo,ColumnName,Created,CreatedBy,DDL_NoForeignKey,Description,EntityType,FieldLength,Help,IsActive,IsAdvancedText,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsCalculated,IsDimension,IsEncrypted,IsGenericZoomKeyColumn,IsGenericZoomOrigin,IsIdentifier,IsKey,IsLazyLoading,IsMandatory,IsParent,IsSelectionColumn,IsStaleable,IsSyncDatabase,IsTranslated,IsUpdateable,IsUseDocSequence,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,555010,541983,0,10,540644,'N','EDIReceiverIdentification',TO_TIMESTAMP('2016-08-23 11:40:03','YYYY-MM-DD HH24:MI:SS'),100,'N','EDI-GLN des Empf√§ngers','de.metas.esb.edi',14,'Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.','Y','N','Y','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','Y','N','Empf√§nger-GLN',0,TO_TIMESTAMP('2016-08-23 11:40:03','YYYY-MM-DD HH24:MI:SS'),100,0)
 ;
 
 -- 23.08.2016 11:40
@@ -115,7 +115,7 @@ INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTran
 
 -- 23.08.2016 11:51
 -- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IncludedTabHeight,IsActive,IsCentrallyMaintained,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,555010,557197,0,540662,0,TO_TIMESTAMP('2016-08-23 11:51:56','YYYY-MM-DD HH24:MI:SS'),100,'EDI-GLN des Empf‰ngers',0,'de.metas.esb.edi','Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.',0,'Y','Y','Y','Y','N','N','N','Y','Y','Empf‰nger-GLN',35,45,0,1,1,TO_TIMESTAMP('2016-08-23 11:51:56','YYYY-MM-DD HH24:MI:SS'),100)
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,ColumnDisplayLength,Created,CreatedBy,Description,DisplayLength,EntityType,Help,IncludedTabHeight,IsActive,IsCentrallyMaintained,IsDisplayed,IsDisplayedGrid,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,SeqNo,SeqNoGrid,SortNo,SpanX,SpanY,Updated,UpdatedBy) VALUES (0,555010,557197,0,540662,0,TO_TIMESTAMP('2016-08-23 11:51:56','YYYY-MM-DD HH24:MI:SS'),100,'EDI-GLN des Empf√§ngers',0,'de.metas.esb.edi','Der Wert ist in erster Linie Identifkator einer EDI-Partei und nicht GLN eines Ortes.',0,'Y','Y','Y','Y','N','N','N','Y','Y','Empf√§nger-GLN',35,45,0,1,1,TO_TIMESTAMP('2016-08-23 11:51:56','YYYY-MM-DD HH24:MI:SS'),100)
 ;
 
 -- 23.08.2016 11:51
